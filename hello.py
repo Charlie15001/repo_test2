ebef6e1e302966c7e1230a2a98acb517582b899d
Charlie <charlie15001@gmail.com>
2020-07-24 22:20:00 +0800

    import selenium to hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print('hello python & git rock')
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+
+api_raininfo = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-E9CFB481-091B-449F-9A6A-F0EA269D8DB6"
+
+driver.get(api_raininfo)
